c820ea08a067dab5f08e52842aec749d415a1804
Ahmed Omosanya <30514950+ah-med@users.noreply.github.com>
2025-05-31 15:38:43 +0100

    configure pretter
    Signed-off-by: Ahmed Omosanya <30514950+ah-med@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    },
+  },
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**'],
+  },
 ];
 
 export default eslintConfig;
